7193c6cbe0d55acea76d2cae0bdae952d351af3e
VolovykAndriy <91112412+VolovykAndriy@users.noreply.github.com>
2023-12-19 23:07:26 +0200

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,39 +1,40 @@
 CREATE TABLE Platform
 (
-  platform_id SERIAL PRIMARY KEY,
-  Platform_Name VARCHAR(50) NOT NULL
+  platform_id INT NOT NULL,
+  Platform_Name VARCHAR(200) NOT NULL,
+  PRIMARY KEY (platform_id)
 );
-
 
 CREATE TABLE Publisher
 (
-  publisher_id SERIAL PRIMARY KEY,
-  Publisher_Name VARCHAR(200) NOT NULL
+  publisher_id INT NOT NULL,
+  Publisher_Name VARCHAR(200) NOT NULL,
+  PRIMARY KEY (publisher_id)
 );
-
-
-CREATE TABLE global_sales
-(
-  gs_id SERIAL PRIMARY KEY,
-  sales DECIMAL(10, 2) NOT NULL,
-  year_of_update INT NOT NULL
-);
-
 
 CREATE TABLE Game
 (
-  game_id SERIAL PRIMARY KEY,
-  Name VARCHAR(100) NOT NULL,
-  Year INT NOT NULL,
-  Genre VARCHAR(50) NOT NULL,
+  game_id INT NOT NULL,
+  Name VARCHAR(350) NOT NULL,
+  Year INT NOT NULL CHECK (Year >= 1900 AND Year <= 9999),
+  Genre VARCHAR(150) NOT NULL,
   platform_id INT NOT NULL,
   publisher_id INT NOT NULL,
-  gs_id INT NOT NULL,
+  PRIMARY KEY (game_id),
   FOREIGN KEY (platform_id) REFERENCES Platform(platform_id),
-  FOREIGN KEY (publisher_id) REFERENCES Publisher(publisher_id),
-  FOREIGN KEY (gs_id) REFERENCES global_sales(gs_id)
+  FOREIGN KEY (publisher_id) REFERENCES Publisher(publisher_id)
 );
 
+CREATE TABLE global_sales
+(
+  gs_id INT NOT NULL,
+  game_id INT NOT NULL,
+  sales FLOAT NOT NULL CHECK (sales >= 0),
+  year_of_update INT NOT NULL CHECK (year_of_update >= 1900 AND year_of_update <= 9999),
+  PRIMARY KEY (gs_id),
+  FOREIGN KEY (game_id) REFERENCES Game(game_id),
+  CONSTRAINT fk_game FOREIGN KEY (game_id) REFERENCES Game(game_id)
+);
 
 CREATE TABLE Platform_has_game
 (
