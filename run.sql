8101ab3176212188852f3989792f334350d913b4
VolovykAndriy <91112412+VolovykAndriy@users.noreply.github.com>
2023-12-19 23:27:50 +0200

    Update run.sql

--- run.sql
+++ run.sql
@@ -1,20 +1,11 @@
--- Виклик функції для отримання кількості ігор для платформи 'PC'
-SELECT get_game_count_for_platform('PC') AS game_count;
+-- Виклик функції
+SELECT get_average_sales('FIFA 22', 2022, 2023) AS average_sales;
 
--- Виклик процедури для оновлення року видання гри з ідентифікатором 1 на 2025
-CALL update_game_year(1, 2025);
+-- Виклик процедури
+CALL update_sales_for_game('FIFA 22', 2022, 15.0);
 
--- Виклик процедури для оновлення інформації про глобальні продажі гри з ідентифікатором 1
-CALL update_game_sales(1, 1500000.50, 2023);
+-- Перевірка роботи тригера (вставка нового запису)
+INSERT INTO global_sales (gs_id, game_id, sales, year_of_update) VALUES (12, 1, 10.0, 2023);
 
--- Вставка нового запису в таблицю Game
-INSERT INTO Game (Name, Year, Genre, platform_id, publisher_id, gs_id)
-VALUES ('New Game', 2020, 'Action', 4, 2, 1);
-
--- Виклик тригера, який автоматично оновлює року оновлення глобальних продаж при зміні суми продажів
-UPDATE global_sales
-SET sales = 2000000.75
-WHERE gs_id = 1
-
--- Виклик функції для отримання кількості ігор для платформи 'PC' після оновлення
-SELECT get_game_count_for_platform('PC') AS game_count_after_update;
+-- Перегляд логу змін
+SELECT * FROM change_log;
