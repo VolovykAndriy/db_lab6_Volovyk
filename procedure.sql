91ddd0bbf54cd1a8b63b80502c468807c212712c
VolovykAndriy <91112412+VolovykAndriy@users.noreply.github.com>
2023-12-19 23:17:59 +0200

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -1,15 +1,22 @@
--- Процедура для оновлення року продажів та суми продажів гри за її ідентифікатором
-CREATE OR REPLACE PROCEDURE update_game_sales(p_game_id INT, p_new_sales FLOAT, p_new_year_of_update INT)
-LANGUAGE plpgsql AS $$
+-- Ця процедура приймає назву гри, рік та нову кількість продажів. 
+-- Вона оновлює кількість продажів для вказаної гри та року. Якщо для даної гри та року немає запису, то процедура вставляє новий запис.
+-- Створення процедури
+CREATE OR REPLACE PROCEDURE update_sales_for_game(game_name VARCHAR(350), update_year INT, new_sales FLOAT)
+LANGUAGE PLPGSQL AS $$
 BEGIN
-  UPDATE global_sales
-  SET sales = p_new_sales,
-      year_of_update = p_new_year_of_update
-  WHERE gs_id = (SELECT gs_id FROM Game WHERE game_id = p_game_id);
+    -- Оновлення кількості продажів для вказаної гри та року
+    UPDATE global_sales
+    SET sales = new_sales
+    WHERE game_id = (SELECT game_id FROM game WHERE Name = game_name)
+      AND year_of_update = update_year;
 
-  COMMIT;
+    -- Якщо не знайдено записів для оновлення, вставити новий запис
+    IF NOT FOUND THEN
+        INSERT INTO global_sales (game_id, sales, year_of_update)
+        VALUES ((SELECT game_id FROM game WHERE Name = game_name), new_sales, update_year);
+    END IF;
 END;
 $$;
 
-CALL update_game_sales(1, 1500000.70, 2023);
-
+-- Виклик процедури для перевірки
+CALL update_sales_for_game('FIFA 22', 2022, 12.5);
