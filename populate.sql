f9743ce3d2ac0f2360f9baf6ef6edb900cc6ae79
VolovykAndriy <91112412+VolovykAndriy@users.noreply.github.com>
2023-12-20 17:17:10 +0200

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,64 +1,83 @@
--- Додавання даних до таблиці Platform
-INSERT INTO Platform (platform_id, Platform_Name) VALUES
-(1, 'PlayStation 4'),
-(2, 'Xbox One'),
-(3, 'Nintendo Switch'),
-(4, 'PC'),
-(5, 'PlayStation 5'),
-(6, 'Xbox Series X'),
-(7, 'Nintendo 3DS'),
-(8, 'Mobile'),
-(9, 'PlayStation Portable'),
-(10, 'Xbox 360');
+-- Заповнення таблиці Platform
+INSERT INTO Platform (platform_id, Platform_Name)
+VALUES
+  (1, 'Wii'),
+  (2, 'NES'),
+  (3, 'DS'),
+  (4, 'GB'),
+  (5, 'X360'),
+  (6, 'PS3'),
+  (7, 'PS2'),
+  (8, 'SNES'),
+  (9, 'GBA'),
+  (10, 'PS'),
+  (11, '3DS'),
+  (12, 'N64');
 
--- Додавання даних до таблиці Publisher
-INSERT INTO Publisher (publisher_id, Publisher_Name) VALUES
-(1, 'Electronic Arts'),
-(2, 'Ubisoft'),
-(3, 'Activision'),
-(4, 'Nintendo'),
-(5, 'Sony Interactive Entertainment'),
-(6, 'Microsoft Studios'),
-(7, 'Square Enix'),
-(8, 'Take-Two Interactive'),
-(9, 'Capcom'),
-(10, 'Sega');
+-- Заповнення таблиці Publisher
+INSERT INTO Publisher (publisher_id, Publisher_Name)
+VALUES
+  (1, 'Nintendo'),
+  (2, 'Microsoft Game Studios'),
+  (3, 'Take-Two Interactive'),
+  (4, 'Sony Computer Entertainment'),
+  (5, 'Activision');
 
--- Додавання даних до таблиці Game
-INSERT INTO Game (game_id, Name, Year, Genre, platform_id, publisher_id) VALUES
-(1, 'FIFA 22', 2021, 'Sports', 1, 1),
-(2, 'Assassin''s Creed Valhalla', 2020, 'Action-Adventure', 2, 2),
-(3, 'Call of Duty: Warzone', 2020, 'Battle Royale', 4, 3),
-(4, 'The Legend of Zelda: Breath of the Wild', 2017, 'Action-Adventure', 3, 4),
-(5, 'Spider-Man: Miles Morales', 2020, 'Action-Adventure', 5, 5),
-(6, 'Halo Infinite', 2021, 'First-Person Shooter', 6, 6),
-(7, 'Final Fantasy VII Remake', 2020, 'Role-Playing', 1, 7),
-(8, 'Grand Theft Auto V', 2013, 'Action-Adventure', 4, 8),
-(9, 'Resident Evil Village', 2021, 'Survival Horror', 7, 9),
-(10, 'Sonic Mania', 2017, 'Platform', 8, 10);
+-- Заповнення таблиці Game
+INSERT INTO Game (game_id, Name, Year, Genre, publisher_id)
+VALUES
+  (1, 'Wii Sports', 2006, 'Sports', 1),
+  (2, 'Super Mario Bros.', 1985, 'Platform', 2),
+  (3, 'Mario Kart Wii', 2008, 'Racing', 3),
+  (4, 'Wii Sports Resort', 2009, 'Sports', 4),
+  (5, 'Pokemon Red/Pokemon Blue', 1996, 'Role-Playing', 5),
+  (6, 'Tetris', 1989, 'Puzzle', 4),
+  (7, 'New Super Mario Bros.', 2006, 'Platform', 1),
+  (8, 'Wii Play', 2006, 'Misc', 4),
+  (9, 'New Super Mario Bros. Wii', 2009, 'Platform', 2),
+  (10, 'Duck Hunt', 1984, 'Shooter', 1),
+  (11, 'Nintendogs', 2005, 'Simulation', 1),
+  (12, 'Mario Kart DS', 2005, 'Racing', 3),
+  (13, 'Pokemon Gold/Pokemon Silver', 1999, 'Role-Playing', 1),
+  (14, 'Wii Fit', 2007, 'Sports', 5),
+  (15, 'Wii Fit Plus', 2009, 'Sports', 1);
+  
+-- Заповнення таблиці global_sales
+INSERT INTO global_sales (gs_id, game_id, sales, year_of_update)
+VALUES
+  (1, 1, 82.74, 2006),
+  (2, 2, 40.24, 1985),
+  (3, 3, 35.82, 2008),
+  (4, 4, 33, 2009),
+  (5, 5, 31.37, 1996),
+  (6, 6, 30.26, 1989),
+  (7, 7, 30.01, 2006),
+  (8, 8, 29.02, 2006),
+  (9, 9, 28.62, 2009),
+  (10, 10, 28.31, 1984),
+  (11, 11, 24.76, 2005),
+  (12, 12, 23.42, 2005),
+  (13, 13, 23.1, 1999),
+  (14, 14, 22.72, 2007),
+  (15, 15, 22, 2009);
 
--- Додавання даних до таблиці global_sales
-INSERT INTO global_sales (gs_id, game_id, sales, year_of_update) VALUES
-(1, 1, 10.5, 2022),
-(2, 2, 15.2, 2022),
-(3, 3, 5.8, 2022),
-(4, 4, 23.5, 2022),
-(5, 5, 8.7, 2022),
-(6, 6, 12.3, 2022),
-(7, 7, 9.1, 2022),
-(8, 8, 140.0, 2022),
-(9, 9, 7.4, 2022),
-(10, 10, 3.6, 2022);
 
--- Додавання даних до таблиці Platform_has_game
-INSERT INTO Platform_has_game (game_id, platform_id) VALUES
-(1, 1),
-(2, 2),
-(3, 4),
-(4, 3),
-(5, 5),
-(6, 6),
-(7, 1),
-(8, 4),
-(9, 7),
-(10, 8);
+  
+-- Заповнення таблиці Platform_has_game
+INSERT INTO Platform_has_game (game_id, platform_id)
+VALUES
+  (1, 1),
+  (2, 2),
+  (3, 1),
+  (4, 1),
+  (5, 4),
+  (6, 4),
+  (7, 3),
+  (8, 1),
+  (9, 1),
+  (10, 2),
+  (11, 3),
+  (12, 3),
+  (13, 4),
+  (14, 1),
+  (15, 1);
